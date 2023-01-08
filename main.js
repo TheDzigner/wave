cf6702fb421f4efc16e1249c1ff0a932d8f013e7
TheDzigner <83682078+TheDzigner@users.noreply.github.com>
2023-01-08 15:08:22 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ Array.from(document.querySelectorAll('.album_wrapper .album_card')
 let albumRoots = ['/culture 3.html','/Only_Built_for_Infinity_Links.html','/god-did.html','/her-lost.html']
 
 
-
 let PlaylistRoot = ['migos-mix.html','quavo-mix.html','takoff-mix.html','offset-mix.html','lil-baby-mix.html']
+
 
 
 allAlbums.forEach(cardAlbum => {
@@ -22,3 +22,24 @@ allCards.forEach(cardplaylist => {
     window.open(PlaylistRoot[allCards.indexOf(this)])
   })
 })
+
+
+const username =
+document.querySelector('.username input')
+
+const UsernameFromStorage = 
+localStorage.getItem('username')
+
+function showUserName()
+{
+  if (UsernameFromStorage == null) {
+    username.value = 'John Doe'
+  } else {
+   username.value = 
+   UsernameFromStorage
+  }
+  
+}
+
+showUserName()
+
